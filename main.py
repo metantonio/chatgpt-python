e1144b1774921dc226d0257a208feb485b60acb8
unknown <antonio_martinez88@hotmail.com>
2023-11-14 10:59:25 -0500

    upd

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import openai
+
+openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+#models
+test_model = "text-davinci-003"
+chatgpt_3 = "gpt-3.5-turbo"
+chatgpt_4 = "gpt-4-1106-preview"
+
+
+if __name__ == '__main__':
+    while True:
+        prompt = input("\n Introduce el prompt a chatgpt-3 o 'exit' para salir\n \n")
+
+        if prompt == "exit":
+            break
+
+        completation = openai.Completion.create(engine=chatgpt_4,
+                                prompt=prompt,
+                                n=1,
+                                max_tokens=2048)
+
+        print(completation.choices[0].text)
