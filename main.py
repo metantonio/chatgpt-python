6c27140cb00c76ae6b33618f6c35e6f7fafa1870
unknown <antonio_martinez88@hotmail.com>
2023-11-14 11:07:18 -0500

    upd

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import os
 import openai
 
-openai.api_key = os.environ.get('OPENAI_API_KEY')
+openai.api_key = os.environ.get("OPENAI_API_KEY")
 
-#models
-test_model = "text-davinci-003"
-chatgpt_3 = "gpt-3.5-turbo"
-chatgpt_4 = "gpt-4-1106-preview"
+# models
+test_model = {"name": "text-davinci-003", "tokens": 2048}
+chatgpt_3 = {"name": "gpt-3.5-turbo", "tokens": 4096}
+chatgpt_4 = {"name": "gpt-4-1106-preview", "tokens": 128000}
 
+#Select Model to use:
+model_selected = chatgpt_4
 
-if __name__ == '__main__':
-    print("""\n Main Menu
+if __name__ == "__main__":
+    print(
+        """\n Main Menu
 
     Menú:
 
@@ -21,19 +24,20 @@ if __name__ == '__main__':
 
     Script: Antonio Martínez
     @metantonio
-    """)
-    userOp1=int(input("\n Choose an option \n"))
+    """
+    )
+    userOp1 = int(input("\n Choose an option \n"))
 
-
-    while True:
+    while userOp1 < 6:
         prompt = input("\n Introduce el prompt a chatgpt-3 o 'exit' para salir\n \n")
 
         if prompt == "exit":
             break
 
-        completation = openai.Completion.create(engine=chatgpt_4,
-                                prompt=prompt,
-                                n=1,
-                                max_tokens=2048)
+        completation = openai.Completion.create(
+            engine=model_selected["name"], prompt=prompt, n=1, max_tokens=model_selected["tokens"]
+        )
 
         print(completation.choices[0].text)
+
+        userOp1 = int(input("\n Choose an option \n"))
